84a7f364e012c89f27d0786263399d554a930167
Arose Niazi <arose.niazi@yahoo.com>
2021-04-20 17:57:37 +0500

    Main Menu, Hosting Design Complete
    Main Menu, Hosting completed the basic design, added script functions. Not implemented yet.

---
+++ Assets/Scripts/Hosting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Hosting : MonoBehaviour
+{
+    [SerializeField] public Button[] players;
+
+    private Text[] _playersName;
+
+    public void Start()
+    {
+        _playersName = new Text[players.Length];
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            _playersName[i] = players[i].GetComponentInChildren<Text>();
+        }
+
+        _playersName[2].text = "New Player";
+        Debug.Log(_playersName[3].text);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Start Game Pressed");
+    }
+
+    public void KickPlayer(int value)
+    {
+        Debug.Log("Kick Player " + value);
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Join()
+    {
+        Debug.Log("Join CALLED");
+    }
+    
+    public void Host()
+    {
+        SceneManager.LoadScene("Hosting");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
